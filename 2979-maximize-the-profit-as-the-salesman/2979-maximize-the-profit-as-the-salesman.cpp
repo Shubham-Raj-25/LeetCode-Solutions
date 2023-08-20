ac259a4471534197b47736c944578850bd18b664
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-20 15:14:52 +0530

    Time: 441 ms (100%), Space: 156.5 MB (50%) - LeetHub

---
+++ 2979-maximize-the-profit-as-the-salesman/2979-maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        vector<int> dp(n + 1);
+        vector<vector<vector<int>>> m(n);
+        for (auto& a : offers) {
+            m[a[1]].push_back(a);
+        }
+        for (int e = 1; e <= n; e++) {
+            dp[e] = dp[e - 1];
+            for (auto& a : m[e - 1]) {
+                dp[e] = max(dp[e], dp[a[0]] + a[2]);
+            }
+        }
+        return dp[n];
+    }
+};
