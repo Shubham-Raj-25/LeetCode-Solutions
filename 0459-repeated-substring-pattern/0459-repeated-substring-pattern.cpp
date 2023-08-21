8dae9d09a907ae22d1e057a71b634d747464fd95
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-21 08:27:57 +0530

    Time: 21 ms (50.61%), Space: 13.5 MB (57.6%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
-        // brute force
-        if(s.length()==1)
-            return false;
-        
-        string temp = "";
-        for(int i=0;i<s.length()/2;i++){
-            temp = s.substr(0,i+1);
-            // cout << temp << endl;
-            bool check = true;
-            for(int k=i+1;k<s.length();k+=temp.length()){
-                if(s.substr(k,temp.length())!=temp){
-                    // cout << "here" << endl;
-                    check = false;
-                    break;
-                }
-            }
-            if(check)
-                return true;
-        }
-
-        return false;
+    bool repeatedSubstringPattern(string temp) {
+        // use trick for O(n) solution
+        string s = temp;
+        s += s;
+        s = s.substr(1,2*temp.length()-2);
+        size_t foundPos = s.find(temp);
+        return foundPos != std::string::npos;
     }
 };
