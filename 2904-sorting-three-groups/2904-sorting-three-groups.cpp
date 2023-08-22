ef8152f1513fff72f82754600fc1fcda2ffd268a
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-22 21:56:50 +0530

    Time: 118 ms (39.33%), Space: 107.3 MB (5.25%) - LeetHub

--- 2904-sorting-three-groups/2904-sorting-three-groups.cpp
+++ 2904-sorting-three-groups/2904-sorting-three-groups.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int func(int ind, int prev, int n, vector<int>& nums,vector<vector<int>>&dp){
+    int func(int idx,int last,vector<int>&nums, vector<vector<int>>& dp){
 
-    if(ind == n) return 0;
+        if(idx==nums.size())
+            return 0;
+        
+        if(dp[idx][last+1]!=-1)
+            return dp[idx][last+1];
 
-    if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+        int notTake = func(idx+1,last,nums,dp);
 
-    int notTake = func(ind+1,prev,n,nums,dp);
-    int take = -1;
-    if(prev==-1 || nums[ind]>=nums[prev])
-        take = 1+func(ind+1,ind,n,nums,dp);
+        int take = 0;
 
-    return dp[ind][prev+1] = max(take,notTake);
+        if(last==-1 || nums[idx]>=nums[last])
+            take = 1+func(idx+1,idx,nums,dp);
 
+        return dp[idx][last+1] = max(take,notTake);    
+    
     }
+
 
     int minimumOperations(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return n-func(0,-1,n,nums,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+3,-1));
+        return n-func(0,-1,nums,dp);
     }
 };
