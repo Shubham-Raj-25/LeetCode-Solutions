274e0b953b56cecb99a3a2ae5c8f660852c8da09
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-23 09:10:00 +0530

    Time: 13 ms (32.27%), Space: 10.8 MB (22.88%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        // o(1) SC later
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+            mp[it]++;
+        vector<int> ans;
+        for(auto it:mp)
+            if(it.second==1)
+                ans.push_back(it.first);
+        
+        return ans;
+    }
+};
