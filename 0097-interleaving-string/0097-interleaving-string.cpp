abecdf822ecf861be8b18d2882111c1126e8f614
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-25 08:14:42 +0530

    Time: 29 ms (30.14%), Space: 31.9 MB (24.1%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    bool func(int i, int j, string temp, string s1, string s2, string s3, vector<vector<int>>& dp) {
-        if (temp == s3) {
+    bool func(int i, int j, string temp, string s1, string s2, string s3, vector<vector<int>>& dp){
+
+        if(temp == s3)
             return true;
-        }
         
-        if (i >= s1.length()) {
+        if(i==s1.length())
             return temp + s2.substr(j) == s3;
-        }
         
-        if (j >= s2.length()) {
+        if(j==s2.length())
             return temp + s1.substr(i) == s3;
-        }
-        
-        if (dp[i][j] != -1) {
+
+        if(dp[i][j]!=-1)
             return dp[i][j];
-        }
         
-        bool result = false;
-        if (s1[i] == s3[i + j]) {
-            result = result || func(i + 1, j, temp + s1[i], s1, s2, s3, dp);
-        }
-        if (s2[j] == s3[i + j]) {
-            result = result || func(i, j + 1, temp + s2[j], s1, s2, s3, dp);
-        }
+        bool ans = false;
+        //trick here is to take one element at a time, not substr
+
+        if(s1[i] == s3[i+j])
+            ans = ans || func(i+1,j,temp+s1[i],s1,s2,s3,dp);
+            
+        if(s2[j] == s3[i+j])
+            ans = ans || func(i,j+1,temp+s2[j],s1,s2,s3,dp);
         
-        dp[i][j] = result;
-        return dp[i][j];
+        return dp[i][j] = ans;
+
     }
 
     bool isInterleave(string s1, string s2, string s3) {
-        if (s1.length() + s2.length() != s3.length()) {
+
+        if(s1.length() + s2.length() != s3.length())
             return false;
-        }
         
-        vector<vector<int>> dp(s1.length() + 1, vector<int>(s2.length() + 1, -1));
-        return func(0, 0, "", s1, s2, s3, dp);
+        int n = s1.length();
+        int m = s2.length();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+
+        return func(0,0,"",s1,s2,s3,dp);
     }
 };
