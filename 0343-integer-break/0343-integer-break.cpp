805237c0be84693c7f2c2b5aea4cd7cfc823bd5f
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-08-25 20:54:08 +0530

    Time: 0 ms (100%), Space: 5.8 MB (97.02%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    
-    map<pair<int,long long>,int> mp;
+    int integerBreak(int n) {
+        // math solution
+        // keep maximum 3s
+        if(n==2) return 1;
+        if(n==3) return 2;
 
-    long long func(int n, int sum, long long prod){
-        if(sum==n)
-            return prod;
-        
-        if(sum > n)
-            return 1;
-        
-        if(mp.find({sum,prod})!=mp.end())
-            return mp[{sum,prod}];
+        int prod = 1;
 
-        long long ans = 1;
-
-        for(int i=1;i<n;i++){
-            ans = max(ans, func(n,sum+i,prod*i));
+        while(n>4){
+            prod *= 3;
+            n -= 3;
         }
 
-        return mp[{sum,prod}] = ans;
-    }
+        prod *= n;
 
-    int integerBreak(int n) {
-        // looks like a dp problem
-        return func(n,0,1);
+        return prod;
     }
 };
