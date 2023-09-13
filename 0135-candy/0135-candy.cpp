1c1f0743a6469abd05d1916629bf5dae8c963aea
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-09-13 08:17:29 +0530

    Time: 51 ms (7.64%), Space: 17.8 MB (31.94%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        // start at minimum positions
+        int n = ratings.size();
+        vector<int> candies(n,1);
+        // Left to right traversal
+        // takes care of the left neighbor
+        for(int i=1;i<n;i++)
+            if(ratings[i]>ratings[i-1])
+                candies[i] = candies[i-1]+1;
+        
+        // Right to left traversal
+        // takes care of the right neighbor
+        for(int i=n-2;i>=0;i--)
+            if(ratings[i]>ratings[i+1])
+                candies[i] = max(candies[i],candies[i+1]+1);
+        
+        int sum = 0;
+
+        for(auto it:candies)
+            {   cout << it << " " ;
+                sum += it;}
+        
+        return sum;
+        
+    }
+};
