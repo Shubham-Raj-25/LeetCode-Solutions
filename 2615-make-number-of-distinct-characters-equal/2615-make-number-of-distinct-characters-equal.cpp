91874deb5b07fbca805f39f5b91ecad64ecc7f4c
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-09-22 00:30:59 +0530

    Time: 77 ms (48.03%), Space: 17.7 MB (14.85%) - LeetHub

---
+++ 2615-make-number-of-distinct-characters-equal/2615-make-number-of-distinct-characters-equal.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void helper(unordered_map<char,int>& mp, int toAdd, int toRemove){
+        mp[toAdd]++;
+        mp[toRemove]--;
+        if(mp[toRemove]==0)
+            mp.erase(toRemove);
+    }
+
+
+    bool isItPossible(string word1, string word2) {
+        
+        unordered_map<char,int> mp1, mp2;
+
+        for(auto it:word1)
+            mp1[it]++;
+        for(auto it:word2)
+            mp2[it]++;
+        
+        for(char c1='a'; c1<='z';c1++){
+            for(char c2='a';c2<='z';c2++){
+
+                if(mp1.count(c1)==0 || mp2.count(c2)==0) continue;
+                    
+                    helper(mp1,c2,c1);
+                    helper(mp2,c1,c2);
+                    
+                    if(mp1.size()==mp2.size())
+                        return true;
+                    
+                    helper(mp1,c1,c2);
+                    helper(mp2,c2,c1);
+                
+            }
+        }
+
+        return false;
+
+
+    }
+};
