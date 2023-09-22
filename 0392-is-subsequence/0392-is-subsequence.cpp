18bf95667f5b203f99a825efec2219000741ab6f
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-09-22 09:35:54 +0530

    Time: 3 ms (49.1%), Space: 6.7 MB (17.6%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    map<pair<int,int>,bool> mp;
-
-    bool func(int i, int j, string s, string t){
-        if(i==s.length())
-            return true;
-
-        if(j==t.length())
-            return false;
-        
-        if(mp.find({i,j})!=mp.end())
-            return mp[{i,j}];
-
-        // take
-        bool take = false, notTake = false;
-
-        if(s[i]==t[j])
-            take = func(i+1,j+1,s,t);
-
-        // not Take
-        notTake = func(i,j+1,s,t);
-
-        return mp[{i,j}] = take||notTake;
-
-    }
-
     bool isSubsequence(string s, string t) {
-        return func(0,0,s,t);
+        int i=0, j=0;
+
+        while(i<s.length() && j<t.length()){
+            if(s[i]==t[j])
+                i++;
+            j++;
+        }
+
+        return i==s.length();
+        
     }
 };
