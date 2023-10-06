bd63b08cf1bc74541acec6c2d9917aab75ff2f60
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-10-06 07:39:35 +0530

    Time: 0 ms (100%), Space: 6.2 MB (53.64%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        // math solution
-        // keep maximum 3s
-        if(n==2) return 1;
-        if(n==3) return 2;
+        // we need to break it into 3s mainly and at last 4
+        if(n==2 || n==3) return n-1;
 
-        int prod = 1;
+        int ans = 1;
 
         while(n>4){
-            prod *= 3;
             n -= 3;
+            ans *= 3;
         }
 
-        prod *= n;
+        ans*=n;
 
-        return prod;
+        return ans;
     }
 };
