b79dc6c25f6d1e0a82ad3b7134732a74930a6658
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2023-10-23 23:51:45 +0530

    Time: 222 ms (28.61%), Space: 63.9 MB (23.72%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1] < b[1] ;
+    }
+    
+    int scheduleCourse(vector<vector<int>>& courses) {
+        
+        sort(courses.begin(),courses.end(),cmp);
+        
+        priority_queue<int> pq;
+
+        int time = 0;
+
+        for(auto it: courses){
+            time += it[0];
+            pq.push(it[0]);
+
+            if(time > it[1]){
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return pq.size();
+    }
+};
