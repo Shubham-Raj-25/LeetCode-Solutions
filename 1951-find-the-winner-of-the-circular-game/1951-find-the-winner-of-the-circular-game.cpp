2f2218944df0343d4e061dcd0bed2a19f94f54ec
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-07-08 22:15:30 +0530

    Time: 3 ms (65.36%), Space: 7 MB (92.77%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> q;
-        for(int i=1;i<=n;i++) q.push(i);
-
-        while(q.size() > 1){
-            int temp = k;
-            while(1){
-                int curr = q.front();
-                q.pop();
-                temp --;
-                if(!temp){
-                    break;
-                }else{
-                    q.push(curr);
-                }
-            }
-        }
-
-        return q.front();
+        // Josephus solution
+        int ans = 0;
+        for(int i=1;i<=n;i++)
+            ans = (ans + k) % i;
+        return ans+1;
     }
 };
