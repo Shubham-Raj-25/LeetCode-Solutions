f82586309645f5ca40f3e8badc96f82fed8ccd4b
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-07-20 08:38:55 +0530

    Time: 59 ms (5.64%), Space: 52.4 MB (5.19%) - LeetHub

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        // will greedy work?
+        int m = rowSum.size(), n = colSum.size();
+        vector<vector<int>> ans(m,vector<int>(n));
+        vector<vector<int>> RunningRowSum(m,vector<int>(n));
+        vector<vector<int>> RunningColSum(m,vector<int>(n));
+
+        for(int j=0;j<n;j++){
+            for(int i=0;i<m;i++){
+                // greedily put maximum val
+                int val = min(rowSum[i],colSum[j]);
+                if(j!=0)
+                    val = min(val,rowSum[i]-RunningRowSum[i][j-1]);
+                if(i!=0)
+                    val = min(val,colSum[j]-RunningColSum[i-1][j]);
+                
+                ans[i][j] = max(val,0);
+                
+                if(j!=0)
+                    RunningRowSum[i][j] = RunningRowSum[i][j-1] + val;
+                else
+                    RunningRowSum[i][j] = val;
+                
+                if(i!=0)
+                    RunningColSum[i][j] = RunningColSum[i-1][j] + val;
+                else
+                    RunningColSum[i][j] = val;
+
+            }
+        }
+
+        return ans;
+
+    }
+};
