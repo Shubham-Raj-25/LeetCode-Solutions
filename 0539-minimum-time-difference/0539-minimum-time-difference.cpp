b7ed2ec833059bbb346c49e302ec7907d0dabd1d
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-09-16 08:42:15 +0530

    Time: 15 ms (31.48%), Space: 17.6 MB (60.05%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -10,16 +10,15 @@ public:
             int minutes = stoi(t.substr(3,2));
             int data = hour * 60 + minutes;
             time.push_back(data);
-            // time.push_back(24*60 - data);
         }
         
         sort(time.begin(),time.end());
 
-        for(int i=0;i<time.size();i++)
-            for(int j=1;j<time.size();j++)
-                if(i != j)
-                    ans = min(ans, min(abs(time[i] - time[j]), 24*60 - abs(time[i] - time[j])));
+        for(int i=1;i<time.size();i++)
+                ans = min(ans, min(abs(time[i] - time[i-1]), 24*60 - abs(time[i] - time[i-1])));
         
+        ans = min(ans, min(abs(time[0] - time.back()), 24*60 - abs(time[0] - time.back())));
+
         return ans;
     }
 };
