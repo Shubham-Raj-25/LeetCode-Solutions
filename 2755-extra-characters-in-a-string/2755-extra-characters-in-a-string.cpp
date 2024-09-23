8b5898a84371303026ae79279b843d2f284a6ea1
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-09-23 08:50:18 +0530

    Time: 324 ms (17.4%), Space: 140.4 MB (16.05%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-
-    int func(int curr, string s, vector<int>& dp, unordered_set<string>& st){
-        if(curr >= s.length())
+    int func(int idx, string s, unordered_set<string>& dict, vector<int>& dp){
+        if(idx == s.length())
             return 0;
         
-        if(dp[curr]!=-1)
-            return dp[curr];
+        if(dp[idx] != -1)
+            return dp[idx];
         
-        dp[curr] = s.length();
+        string temp = "";
 
-        string currStr = "";
+        dp[idx] = s.length();
 
-        for(int cut=curr;cut<s.length();cut++){
-            currStr.push_back(s[cut]);
-            int currExtra = st.find(currStr) == st.end() ? currStr.length() : 0;
-            int futureExtra = func(cut+1,s,dp,st);
-            dp[curr] = min(dp[curr],currExtra+futureExtra);
-        }
-
-        return dp[curr];
+        for(int cut=idx;cut<s.length();cut++){
+            temp.push_back(s[cut]);
+            int currExtra = dict.count(temp) == 0 ? temp.length() : 0;
+            int futureExtra = func(cut+1,s,dict,dp);
+            dp[idx] = min(dp[idx], currExtra + futureExtra);
+        }   
+        return dp[idx];
     }
 
     int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string> dict;
         vector<int> dp(s.length(),-1);
-        unordered_set<string> st;
-        for(auto it:dictionary) st.insert(it);
-        return func(0,s,dp,st);
+        for(string word : dictionary) dict.insert(word);
+        return func(0,s,dict,dp);
     }
 };
