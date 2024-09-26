20b971a05451897e4d111d593b0aa66355ad4f07
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-09-26 09:47:40 +0530

    Time: 9 ms (85.38%), Space: 13.1 MB (85.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int func(int curr, int prev, vector<vector<int>>& dp, vector<int>& nums){
-        if(curr == nums.size())
-            return 0;
-        
-        if(dp[curr][prev+1] != -1)
-            return dp[curr][prev+1];
-        
-        int pick = 0, skip = 0;
-        
-        if(prev == -1 || nums[curr] > nums[prev])
-            pick = 1 + func(curr+1,curr,dp,nums);
-        
-        skip = func(curr+1,prev,dp,nums);
-
-        return dp[curr][prev+1] = max(pick,skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
-        return func(0,-1,dp,nums);
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }else{
+                int idx = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+                temp[idx] = nums[i];
+            }
+        }
+
+        return len;
     }
 };
