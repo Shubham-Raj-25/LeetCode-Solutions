896be2b0e3cd2e5910e4d8eeebfcfcb14019e285
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-02 09:01:27 +0530

    Time: 4 ms (37.99%), Space: 12.9 MB (6.87%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,31 +21,27 @@ public:
 
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
-        if(!node)
-            return NULL;
+    void dfs(Node* node,map<Node*, Node*>& og_to_clone,set<Node*>& vis){
+        if(node == NULL || vis.count(node))
+            return;
         
-        unordered_map<Node*, Node*> clone;
-        queue<Node*> q;
+        vis.insert(node);
 
-        clone[node] = new Node(node->val);
-        q.push(node);
-
-        while(q.size()){
-            Node* curr = q.front();
-            q.pop();
-
-            for(Node* nei : curr->neighbors){
-                
-                if(clone.find(nei) == clone.end()){ // if not visited
-                    clone[nei] = new Node(nei->val);
-                    q.push(nei);
-                }
-
-                clone[curr]->neighbors.push_back(clone[nei]);
-            }
+        if(og_to_clone.find(node) == og_to_clone.end())
+            og_to_clone[node]  = new Node(node->val);
+        
+        for(auto nei : node->neighbors){
+            if(og_to_clone.find(nei) == og_to_clone.end())
+                og_to_clone[nei] = new Node(nei->val);
+            og_to_clone[node]->neighbors.push_back(og_to_clone[nei]);
+            dfs(nei,og_to_clone,vis);
         }
+    }
 
-        return clone[node];
+    Node* cloneGraph(Node* node) {
+        map<Node*, Node*> og_to_clone;
+        set<Node*> vis;
+        dfs(node,og_to_clone,vis);
+        return og_to_clone[node];
     }
 };
