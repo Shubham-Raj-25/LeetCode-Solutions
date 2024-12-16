cd6192ac8613a88fe949d5addf188e30b9df58b4
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-16 13:40:49 +0530

    Time: 6 ms (63.31%), Space: 13.1 MB (34.4%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,61 @@
 class Solution {
 public:
-    int func(int i, int j, string& word1, string& word2,vector<vector<int>>& dp){
-        if(i == word1.length() || j == word2.length())
-            return word1.length() - i + word2.length()-j;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int both_match = INT_MAX, del = INT_MAX, replace = INT_MAX, insert = INT_MAX;
-        if(word1[i] == word2[j])
-            both_match = func(i+1,j+1,word1,word2,dp);
-        else{
-            del = 1+func(i+1,j,word1,word2,dp);
-            replace = 1+func(i+1,j+1,word1,word2,dp);
-            insert = 1 + func(i,j+1,word1,word2,dp);
+    int func(int i,int j,vector<vector<int>>& dp, string& w1, string& w2){
+        if(i == w1.length()){
+            return w2.length() - j;
         }
-        return dp[i][j] = min(both_match, min(del,min(insert,replace)));
+
+        if(j == w2.length()){
+            return w1.length() - i;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int min_operations = INT_MAX;
+
+        if(w1[i] == w2[j]){
+            min_operations = func(i+1,j+1,dp,w1,w2);
+        }
+        else{
+            // inserting
+            int insert_case = 1+func(i,j+1,dp,w1,w2);
+            // deleting
+            int delete_case = 1+func(i+1,j,dp,w1,w2);
+            // replacing
+            int replace_case = 1+func(i+1,j+1,dp,w1,w2);
+            min_operations = min(min_operations, (int)min(insert_case,(int)min(delete_case,replace_case)));
+        }
+
+        return dp[i][j] = min_operations;
     }
 
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.length(), vector<int> (word2.length(),-1));
-        return func(0,0,word1,word2,dp);
+        int n = word1.length();
+        int m = word2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+        for(int j=0;j<=m;j++){
+            dp[n][j] = m-j;
+        }
+
+        for(int i=0;i<=n;i++){
+            dp[i][m] = n-i;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(word1[i] == word2[j]){
+                    dp[i][j]  = dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i+1][j], (int)min(dp[i+1][j+1],dp[i][j+1]));
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
+
