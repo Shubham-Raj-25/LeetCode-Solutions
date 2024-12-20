d4298b00d9ee0131dd8004a078f66c129a69a089
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-20 08:57:33 +0530

    Time: 0 ms (100%), Space: 77.1 MB (74.77%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -11,42 +11,21 @@
  */
 class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        // bfs with a stack and a queue?
-        stack<int> st;
-        queue<TreeNode*> q, temp;
-        int level = 0;
-        q.push(root);
-
-        while(q.size()){
-            int sz = q.size();
-            for(int i=0;i<sz;i++){
-                
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if(curr->left != nullptr){
-                    q.push(curr->left);
-                    q.push(curr->right);
-                }
-                
-                if(level % 2 != 0){
-                    temp.push(curr);
-                    st.push(curr->val);
-                }
-            }
-
-            if(level % 2 != 0){ // reverse the values
-                while(temp.size()){
-                    TreeNode* curr = temp.front();
-                    temp.pop();
-                    curr->val = st.top();
-                    st.pop();
-                }
-            }
-            level ++;
+    void dfs(TreeNode* lft, TreeNode* rt, int level){
+        if(lft == nullptr){
+            return;
         }
-        
+
+        if(level % 2 != 0){ // odd level so swap values
+            swap(lft->val,rt->val);
+        }
+
+        dfs(lft->left,rt->right,level+1);
+        dfs(lft->right,rt->left,level+1);
+    }
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        dfs(root->left,root->right,1);
         return root;
     }
 };
