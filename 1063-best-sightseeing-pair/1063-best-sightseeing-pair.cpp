da6564024dbf20887517354d240d6ab91e4cadc9
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-27 08:09:16 +0530

    Time: 2 ms (33.26%), Space: 45.6 MB (55.58%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        // v1+i + v2-j
-        int n = values.size();
-        vector<int> first(n), second(n);
-        first[0] = values[0]+0;
-        for(int i=1;i<n;i++)
-            first[i] = max(first[i-1],values[i]+i);
-        second[n-1] = values[n-1]-(n-1);
-        for(int j=n-2;j>=0;j--)
-            second[j] = max(second[j+1],values[j]-j);
-        
-        for(int i : first)
-            cout << i << " ";
-        cout << endl;
+        int ans = 0, maxVal = 0;
+        for(int i=0;i<values.size();i++){
+            ans = max(ans, maxVal + values[i] - i);
+            maxVal = max(maxVal, values[i]+i);
+        }
 
-        for(int j : second)
-            cout << j << " ";
-        cout << endl;
-        
-        int maxi = 0;
-        for(int i=0;i<n-1;i++)
-            maxi = max(maxi,first[i]+second[i+1]);
-        
-        return maxi;
+        return ans;
     }
 };
