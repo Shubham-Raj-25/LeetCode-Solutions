6a1bc0a1b79a988c7dc75f43cdbcc0caa5279f56
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-28 10:55:57 +0530

    Time: 3 ms (61.79%), Space: 26.3 MB (61.79%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -1,64 +1,48 @@
 class Solution {
 public:
-    vector<int> ans;
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        int n = nums.size(), maxSum = 0;
+        vector<int> prefixSum(n, 0), leftMaxPos(n, 0), rightMaxPos(n, n - k), ans;
 
-    vector<int> getSubarraySum(vector<int>& arr, int k) {
-        int sum = 0;
-        vector<int> ret;
-        for (int i = 0; i < k; i++)
-            sum += arr[i];
-        
-        ret.push_back(sum);
-
-        for (int r = k, l = 0; r < arr.size(); r++) {
-            sum += arr[r];
-            sum -= arr[l];
-            l++;
-            ret.push_back(sum);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
-        return ret;
-    }
-
-    void path_trace(int idx, int k, int element_left, vector<int>& data, vector<vector<int>>& dp) {
-        if (element_left <= 0)
-            return;
-        else {
-            int pick = data[idx] + dp_func(idx + k, k, element_left - 1, data, dp);
-            int not_pick = dp_func(idx + 1, k, element_left, data, dp);
-            if (pick >= not_pick) {
-                ans.push_back(idx); // as picking this proved beneficial
-                path_trace(idx + k, k, element_left - 1, data, dp);
+        int maxSoFar = prefixSum[k - 1]; 
+        for (int i = k; i < n; i++) {
+            int currWinSum = prefixSum[i] - prefixSum[i - k];
+            if (currWinSum > maxSoFar) {
+                leftMaxPos[i] = i - k + 1;
+                maxSoFar = currWinSum;
             } else {
-                path_trace(idx + 1, k, element_left, data, dp);
+                leftMaxPos[i] = leftMaxPos[i - 1];
             }
         }
-    }
 
-    int dp_func(int idx, int k, int element_left, vector<int>& data, vector<vector<int>>& dp) {
-        if (element_left == 0)
-            return 0;
-
-        if (idx >= data.size()) {
-            if (element_left != 0)
-                return INT_MIN;
-            return 0;
-        } else if (dp[idx][element_left] != -1)
-            return dp[idx][element_left];
-        else {
-            int pick = data[idx] + dp_func(idx + k, k, element_left - 1, data, dp);
-            int not_pick = dp_func(idx + 1, k, element_left, data, dp);
-            return dp[idx][element_left] = max(pick, not_pick);
+        maxSoFar = prefixSum[n - 1] - prefixSum[n - 1 - k]; 
+        for (int i = n - k - 1; i >= 0; i--) {
+            int currWinSum = prefixSum[i + k - 1] - (i > 0 ? prefixSum[i - 1] : 0);
+            if (currWinSum >= maxSoFar) {
+                rightMaxPos[i] = i;
+                maxSoFar = currWinSum;
+            } else {
+                rightMaxPos[i] = rightMaxPos[i + 1];
+            }
         }
-    }
 
-    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
-        // store the sum of all possible subarray and then find by dp        
-        vector<int> data = getSubarraySum(nums, k);
-        int n = data.size();
-        vector<vector<int>> dp(n, vector<int>(4, -1));
-        dp_func(0, k, 3, data, dp);
-        path_trace(0, k, 3, data, dp);
+        for (int i = k; i <= n - 2 * k; i++) {
+            int l = leftMaxPos[i - 1];
+            int r = rightMaxPos[i + k];
+            int total = (prefixSum[i + k - 1] - prefixSum[i - 1]) + 
+                        (prefixSum[l + k - 1] - (l > 0 ? prefixSum[l - 1] : 0)) + 
+                        (prefixSum[r + k - 1] - (r > 0 ? prefixSum[r - 1] : 0));
+            if (total > maxSum) {
+                maxSum = total;
+                ans = {l, i, r};
+            }
+        }
+
         return ans;
     }
 };
