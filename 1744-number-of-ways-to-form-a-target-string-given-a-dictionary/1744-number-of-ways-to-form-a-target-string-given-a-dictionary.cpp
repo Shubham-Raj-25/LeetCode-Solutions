29f95de5da8fada53e00b70420e8b5280289bac8
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2024-12-29 16:27:01 +0530

    Time: 2015 ms (5.11%), Space: 797.5 MB (5.35%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,33 +1,31 @@
 class Solution {
-int mod=1e9+7;
-
-private:
-    long long ex(int i, int j, int n, map<char,int> *hash, string &target, vector<vector<int>> &dp){
-        if(j>=target.size()) return 1;
-        if(i>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        long long ans=0;
-        if(hash[i][target[j]]>0){
-            long long take=hash[i][target[j]]*ex(i+1,j+1,n,hash,target,dp)%mod;
-            ans=(ans+take)%mod;
-        }
-        long long not_take=ex(i+1,j,n,hash,target,dp)%mod;
-        ans=(ans+not_take)%mod;
-
-        return dp[i][j]=ans;
-    }
-
 public:
+    int MOD = 1e9+7;
+    int dp[1002][1002];
+    int mp[1002][27];
     int numWays(vector<string>& words, string target) {
-        map<char,int> hash[words[0].size()];
-        for(int i=0; i<words.size(); i++){
-            for(int j=0; j<words[i].size(); j++){
-                hash[j][words[i][j]]++;
+
+        // preprocess word data.
+        int m = words[0].length();
+        int n = target.length();
+        
+        for(int i=0;i<m;i++){ 
+            for(string word : words){
+                mp[i][word[i] - 'a']++;
             }
         }
 
-        vector<vector<int>> dp(words[0].size(), vector<int>(target.size(),-1));
-        return (int)ex(0,0,words[0].size(),hash,target,dp);
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j] = (1LL * (mp[j-1][target[i-1]-'a'] % MOD) * (dp[i-1][j-1] % MOD) + dp[i][j-1]) % MOD;
+            }
+        }
+
+        return dp[n][m];
+
     }
 };
