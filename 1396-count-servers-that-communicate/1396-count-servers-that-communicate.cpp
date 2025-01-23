d5734004e113009bfcc3664bab9bc19fe3adf398
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2025-01-23 08:20:29 +0530

    Time: 0 ms (100%), Space: 26.6 MB (59.58%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        unordered_map<int,int> row, col;
-        for(int i=0;i<grid.size();i++){
-            int count = 0;
-            for(int j=0;j<grid[0].size();j++)
-                if(grid[i][j]==1)
-                    count++;
-            row[i] = count;
-        }
-
-        for(int i=0;i<grid[0].size();i++){
-            int count = 0;
-            for(int j=0;j<grid.size();j++)
-                if(grid[j][i]==1)
-                    count++;
-            col[i] = count;
-        }
-
-        int ans = 0;
-
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    if(row[i]>1 || col[j]>1)
-                        ans++;
+        int rows = grid.size(), cols = grid[0].size(), ans = 0;
+        vector<int> rowData(rows,0), colData(cols,0);
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    rowData[i]++;
+                    colData[j]++;
                 }
             }
         }
 
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    if(rowData[i] > 1 || colData[j] > 1){
+                        ans++;
+                    }
+                }
+            }
+        }
         return ans;
     }
 };
