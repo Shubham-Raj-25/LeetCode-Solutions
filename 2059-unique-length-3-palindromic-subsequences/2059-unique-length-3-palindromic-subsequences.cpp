dbb9094e3e42ba6dc6ab3c5d0d4f59ba1248ebe4
Shubham Raj <109397172+Shubham-Raj-25@users.noreply.github.com>
2025-11-21 09:03:19 +0530

    Time: 134 ms (83.6%), Space: 15.9 MB (58.2%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> freq(26,0);
-        int ans = 0;
-        
-        for(char c : s){
-            freq[c-'a']++;
+        vector<pair<int,int>> occur(26,{-1,-1});
+        int n = s.length();
+
+        for(int i=0;i<n;i++){
+            if(occur[s[i]-'a'].first==-1) occur[s[i]-'a'].first=i; // will store the first occurance
+            else occur[s[i]-'a'].second = i; // will end up storing the last occurance
         }
 
-        for(char c = 'a'; c <= 'z'; c++){
-            if(freq[c-'a'] < 2){
-                continue;
-            }
-            int i = 0;
-            while(s[i] != c){
-                i++; // first appearance
-            }
-            
-            i++;
+        int ans = 0;
 
-            freq[c-'a']--;
-
-            unordered_set<char>st;
-            while(1){
-                char m = s[i];
-                if(m == c){
-                    freq[c-'a']--;
-                    if(freq[c-'a'] == 0)
-                        break;
-                }
-                st.insert(m);
-                i++;
+        for(int i=0;i<26;i++){
+            if(occur[i].second!=-1){ // means appear a second time
+                unordered_set<char> st;
+                    for(int j=occur[i].first+1;j<=occur[i].second-1;j++) st.insert(s[j]);
+                    ans += st.size();                    
             }
-            ans += st.size();
         }
 
         return ans;
